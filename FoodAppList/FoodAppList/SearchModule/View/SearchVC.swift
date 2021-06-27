123be8285c34f1bea490d362d56339185bd9d902
Grigory <grishavinyar@icloud.com>
2021-06-27 08:00:42 +0300

    Make the extension for UIView
    1. Extensions group
    2. Setup SearchVC

--- FoodAppList/FoodAppList/SearchModule/View/SearchVC.swift
+++ FoodAppList/FoodAppList/SearchModule/View/SearchVC.swift
@@ -9,10 +9,38 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemPink
+        configureTableView()
+    }
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        setTableViewDelegates()
+        tableView.rowHeight = 100
+        // register cell
+        tableView.pin(to: view)
+    }
+    
+    func setTableViewDelegates() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+}
+
+extension SearchVC: UITableViewDelegate {}
+
+extension SearchVC: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
